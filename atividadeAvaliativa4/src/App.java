29da90cad286fc06fb4647e7bbc8e1d661664eab
Victor Hugo <vchugo031@gmail.com>
2026-08-27 17:58:36 +0000

    atividadeAvalliativa4

---
+++ atividadeAvaliativa4/src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        String nome = "Cecilía";
+        int idade = 17;
+        int anosNoFuturo = 5;
+
+        System.out.println("Nome :" + nome);
+        System.out.println("Idade Hoje :" + idade);
+        System.out.println("Idade Futura :" + (idade + anosNoFuturo));
+    }
+}
